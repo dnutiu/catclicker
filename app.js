6a6f66c3b25ea7702de9e1a5f8a9604251d9d61b
Denis <nutiudenis@hotmail.com>
2015-04-30 19:20:48 +0300

    Ok The final thing I promise

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ var model = {
 	currentCatIndex: 0,
 	cats: [
 		{
-			catName: "Luca",
-			catCounter: 0,
-			catImg: "cat_picture1.jpg"
+			name: "Luca",
+			clicks: 0,
+			image: "cat_picture1.jpg"
 		},
 		{
-			catName: "Tutu",
-			catCounter: 0,
-			catImg: "cat_picture2.jpeg"
+			name: "Tutu",
+			clicks: 0,
+			image: "cat_picture2.jpeg"
 		},
 		{
-			catName: "Tommy",
-			catCounter: 0,
-			catImg: "cat_picture3.jpeg"
+			name: "Tommy",
+			clicks: 0,
+			image: "cat_picture3.jpeg"
 		},
 		{
-			catName: "Mita",
-			catCounter: 0,
-			catImg: "cat_picture4.jpeg"
+			name: "Mita",
+			clicks: 0,
+			image: "cat_picture4.jpeg"
 		},
 		{
-			catName: "France",
-			catCounter: 0,
-			catImg: "cat_picture5.jpeg"
+			name: "France",
+			clicks: 0,
+			image: "cat_picture5.jpeg"
 		},
 	]
 };
@@ -42,64 +42,67 @@ var controller = {
 		model.currentCat = cat;
 		model.currentCatIndex = i;
 	},
+	getCurrentCatIndex: function() {
+		return model.currentCatIndex;
+	},
 	getCats: function() {
 		return model.cats;
 	},
-	getCatIndex: function() {
-		return model.currentCatIndex;
+	incrementClicks: function() {
+		model.currentCat.clicks++
 	},
 	adminChangeData: function() {
-		var index = this.getCatIndex();
+		var index = this.getCurrentCatIndex();
 		var name = $('#a-cat-name').val();
 		var image = $('#a-cat-image').val();
 		var clicks = $('#a-cat-clicks').val();
 
 		if (image == "") {
-			image = model.currentCat.catImg;
+			image = model.currentCat.image;
 		} 
 		if (clicks == "") {
-			clicks = model.currentCat.catCounter;
+			clicks = model.currentCat.clicks;
 		}
 		if (name == "") {
-			name = model.currentCat.catName;
+			name = model.currentCat.name;
 		}
 
 		var cat = {
-			catName: name,
-			catCounter: clicks,
-			catImg: image
+			name: name,
+			clicks: clicks,
+			image: image
 		}
 
 		this.setCurrentCat(cat, index);
 		this.getCats()[index] = cat;
 
-		view.drawList();
+		view.drawListArea();
 		view.drawDisplayArea();
-		
+
 		$('#admin-panel').toggle('display');
 	}
 };
 
 var view = {
 	init: function() {
-		this.drawList();
+		this.drawListArea();
 		this.drawDisplayArea();
 		this.drawAdminArea();
 		this.updateAdminArea();
 
 		$('#cat-image').click(function() {
-			model.currentCat.catCounter++;
+			controller.incrementClicks();
 			view.updateCounter();
 		});
 	},
-	drawList: function() {
+	drawListArea: function() {
 		var cats = controller.getCats();
 
 		$('#cat-list').html(' ');
 		$('#cat-list').append("<ul id=\"cat-list-display\"></ul>");
 		for(var i = 0; i < cats.length; i++) {
 			$('#cat-list-display').append(
-				"<li id=" + i + ">" + cats[i].catName + "</li>"
+				"<li id=" + i + ">" + cats[i].name + "</li>"
 				);
 		}
 
@@ -116,9 +119,9 @@ var view = {
 		$('#cat-name').text(' ');
 		$('#cat-counter').text(' ');
 		
-		$('#cat-name').append(cat.catName);
-		$('#cat-counter').append(cat.catCounter)
-		$('#cat-image').attr('src', cat.catImg);
+		$('#cat-name').append(cat.name);
+		$('#cat-counter').append(cat.clicks)
+		$('#cat-image').attr('src', cat.image);
 
 	},
 	drawAdminArea: function() {
@@ -133,18 +136,25 @@ var view = {
 			controller.adminChangeData();
 			view.drawDisplayArea();
 		});
+
+		$('#a-cancel-btn').click(function(e) {
+			e.preventDefault();
+			view.updateAdminArea();
+		});
 	},
 	updateAdminArea: function() {
 		var cat = controller.getCurrentCat();
 
-		var name = $('#a-cat-name').val(cat.catName);
-		var image = $('#a-cat-image').val(cat.catImg);
-		var clicks = $('#a-cat-clicks').val(cat.catCounter);
+		var name = $('#a-cat-name').val(cat.name);
+		var image = $('#a-cat-image').val(cat.image);
+		var clicks = $('#a-cat-clicks').val(cat.clicks);
 	},
 	updateCounter: function() {
 		var cat = controller.getCurrentCat();
+		var clicks = $('#a-cat-clicks').val(cat.clicks);
+		
 		$('#cat-counter').html(' ');
-		$('#cat-counter').append(cat.catCounter)
+		$('#cat-counter').append(cat.clicks)
 	}
 };
 controller.init();
